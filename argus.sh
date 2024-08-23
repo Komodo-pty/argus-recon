417dc39529607d0aa8a8a483a019369c3215482e
Komodo <144389222+Komodo-pty@users.noreply.github.com>
2024-08-23 14:07:22 -0700

    Add files via upload

--- argus.sh
+++ argus.sh
@@ -5,21 +5,25 @@ while true;
 do
 	echo -e "\nSelect an operation:\n[0] Exit\n[1] Port Scan\n[2] Web App\n[3] SMB\n"
 	read mode
+
 	if [ $mode == 0 ]
 	then
 		break
-	fi
-	if [ $mode == 1 ]
+
+	elif [ $mode == 1 ]
 	then
 		source $path/port_scan.sh
-	fi
-	if [ $mode == 2 ]
+
+	elif [ $mode == 2 ]
 	then
 		source $path/web_enum.sh
-	fi
-	if [ $mode == 3 ]
+
+	elif [ $mode == 3 ]
 	then
 		source $path/smb_logins.sh
+
+	else
+		echo -e "\nYou did not select a valid option\n"
 	fi
 
 done
