2e4fed37b430bd38d55807c628cfa7058d117e33
Nathan Miller <144389222+Komodo-pty@users.noreply.github.com>
2025-08-14 16:24:32 -0700

    Arguments for SMB module now supported

--- smb_enum.sh
+++ smb_enum.sh
@@ -2,13 +2,39 @@
 
 line="\n============================================================\n"
 target=""
+mode=""
+ad=0
 
-while getopts ":i:" option; do
-	case $option in
-		i)
-			target="$OPTARG"
-			;;
-	esac
+Help()
+{
+cat <<EOF
+[Options]
+	-h: Display this help menu
+	-i <IP_ADDRESS>: The target's IP Address
+	-x <MODE>: Specify the operation to perform
+
+[Modes]
+	logins: Test default logins
+	users: Enumerate users via Read access to IPC$
+	shares: List contents & permissions for all shares
+	enum: Enumerate server info (e.g. account descriptions)
+
+EOF
+exit 0
+}
+
+while getopts ":hi:x:" option; do
+  case $option in
+    h)
+      Help
+      ;;
+    i)
+      target="$OPTARG"
+      ;;
+    x)
+      mode="$OPTARG"
+      ;;
+  esac
 done
 
 if [[ -z "$target" ]];
@@ -17,230 +43,212 @@ then
 	read target
 fi
 
-echo -e "\nSelect an operation:\n[1] Test default logins\n[2] Enumerate users via Read access to IPC$\n[3] List contents & permissions for all shares"
-echo -e "[4] Enumerate Server Info (e.g. Get Account Descriptions)\n"
+if [[ -z "$mode" ]]; then
+  cat <<EOF
+Select an operation:
+	[1] Test default logins
+	[2] Enumerate users via Read access to IPC$
+	[3] List contents & permissions for all shares
+	[4] Enumerate server info (e.g. account descriptions)
+EOF
+  read mode
+fi
 
-read mode
+if (( mode == 1 || mode == 2 )) || [[ "$mode" == "logins" || "$mode" == "users" ]]; then
+  query=$(nxc smb $target -u '' -p '')
+  domain=$(echo "$query" | grep domain | awk -F 'domain:' '{print $2}' | awk -F ')' '{print $1}')
+  host=$(echo "$query" | awk -F 'name:' '{print $2}' | awk -F ')' '{print $1}')
+  dom="\n[+] Using Domain: $domain\n"
+  hostname="\n[+] Using Hostname: $host\n"
 
-if (( mode == 1 || mode == 2 ))
-then
-	query=$(nxc smb $target -u '' -p '')
-	domain=$(echo "$query" | grep domain | awk -F 'domain:' '{print $2}' | awk -F ')' '{print $1}')
-	host=$(echo "$query" | awk -F 'name:' '{print $2}' | awk -F ')' '{print $1}')
-	dom="\n[+] Using Domain: $domain\n"
-	hostname="\n[+] Using Hostname: $host\n"
-	ad=0
+  if [[ "$host" == "$domain" ]]; then
+    echo -e "\nThe Hostname is identical to the Domain Name\n"
 
-	if [ "$host" == "$domain" ]
-	then
-		echo -e "\nThe Hostname is identical to the Domain Name\n"
+  elif [[ -z "$domain" ]]; then
+    echo -e "\nNo Domain Name detected\n"
 
-	elif [ "$domain" == "" ]
-	then
-		echo -e "\nNo Domain Name detected\n"
-
-	else
-		ad=1
-	fi
+  else
+    ad=1
+  fi
 fi
 echo -e "$line"
 
-if (( mode == 1 ))
-then
-	echo -e "$hostname"
+case "$mode" in
+  logins|1)
+    echo -e "$hostname\nNull Login. No creds\n"
+    nxc smb "$target" -u '' -p '' -d "$host" --shares
+    echo -e "$line"
 
-	echo -e "Null Login. No creds\n"
-	nxc smb "$target" -u '' -p '' -d "$host" --shares
-	echo -e "$line"
+    echo -e "Anonymous username & no password\n"
+    nxc smb "$target" -u 'Anonymous' -p '' -d "$host" --shares
+    echo -e "$line"
 
-	echo -e "Anonymous username & no password\n"
-	nxc smb "$target" -u 'Anonymous' -p '' -d "$host" --shares
-	echo -e "$line"
+    echo -e "Anonymous:Anonymous\n"
+    nxc smb "$target" -u 'Anonymous' -p 'Anonymous' -d "$host" --shares
+    echo -e "$line"
 
-	echo -e "Anonymous:Anonymous\n"
-	nxc smb "$target" -u 'Anonymous' -p 'Anonymous' -d "$host" --shares
-	echo -e "$line"
+    echo -e "Guest login\n"
+    nxc smb "$target" -u 'guest' -p '' -d "$host" --shares
+    echo -e "$line"
 
-	echo -e "Guest login\n"
-	nxc smb "$target" -u 'guest' -p '' -d "$host" --shares
-	echo -e "$line"
+    if (( ad == 1 )); then
+      echo -e "$dom\nNull Login. No creds\n"
+      nxc smb "$target" -u '' -p '' -d "$domain" --shares
+      echo -e "$line"
 
-	if (( ad == 1 ))
-	then
-		echo -e "$dom"
+      echo -e "Anonymous username & no password\n"
+      nxc smb "$target" -u 'Anonymous' -p '' -d "$domain" --shares
+      echo -e "$line"
 
-		echo -e "Null Login. No creds\n"
-		nxc smb "$target" -u '' -p '' -d "$domain" --shares
-		echo -e "$line"
+      echo -e "Anonymous:Anonymous\n"
+      nxc smb "$target" -u 'Anonymous' -p 'Anonymous' -d "$domain" --shares
+      echo -e "$line"
 
-		echo -e "Anonymous username & no password\n"
-		nxc smb "$target" -u 'Anonymous' -p '' -d "$domain" --shares
-		echo -e "$line"
+      echo -e "Guest login\n"
+      nxc smb "$target" -u 'guest' -p '' -d "$domain" --shares
+      echo -e "$line"
+    fi
+    ;;
+  users|2)
+    cat <<EOF
+Do you want to extract usernames & save them to a file?
 
-		echo -e "Anonymous:Anonymous\n"
-		nxc smb "$target" -u 'Anonymous' -p 'Anonymous' -d "$domain" --shares
-		echo -e "$line"
+[1] No. Print results to STDOUT
+[2] Yes. Print results & then write usernames to a file
+EOF
+    read opt
 
-		echo -e "Guest login\n"
-		nxc smb "$target" -u 'guest' -p '' -d "$domain" --shares
-		echo -e "$line"
-	fi
-elif (( mode == 2 ))
-then
-	echo -e "\nDo you want to extract usernames & save them to a file?\n[1] No. Print results to STDOUT\n[2] Yes. Print results & then write usernames to a file\n"
-	read opt
+    if (( opt == 1 || opt == 2 )); then
+      echo -e "\nEnter the Username to use (Use Syntax DOMAIN/USER or HOSTNAME/USER):\n"
+      read user
 
-	if (( opt == 1 || opt == 2 ))
-	then
-		echo -e "\nEnter the Username to use (Use Syntax DOMAIN/USER or HOSTNAME/USER):\n"
-		read user
+      cat <<EOF
+Select an option:
 
-		echo -e "\nSelect an option:\n[1] Authenticate using a password\n[2] Authenticate using an NTLM hash\n[3] Use an account that doesn't have a password\n"
-		read cred
+[1] Authenticate using a password
+[2] Authenticate using an NTLM hash
+[3] Use an account that doesn't have a password
+EOF
+      read cred
 
-		if (( cred == 1 ))
-		then
-			echo -e "\nEnter the Password:\n"
-			read pass
+      if (( cred == 1 )); then
+        echo -e "\nEnter the Password:\n"
+        read pass
 
-		elif (( cred == 2 ))
-		then
-			echo -e "\nEnter the NTLM hash:\n"
-			read ntlm
+      elif (( cred == 2 )); then
+        echo -e "\nEnter the NTLM hash:\n"
+        read ntlm
 
-		elif (( cred == 3 ))
-		then
-			echo -e "\nConnecting using an account with a Null password\n"
-		else
-			echo -e "\nYou did not select a valid option\n"
-			return
-		fi
-	fi
+      elif (( cred == 3 )); then
+        echo -e "\nConnecting using an account with a Null password\n"
+      else
+        echo -e "\nYou did not select a valid option\n"
+        return
+      fi
+    fi
 
-	if (( opt == 1 || opt == 2 ))
-	then
-		if (( cred == 1 ))
-		then
-			echo -e "\n[Local SIDs]\n"
-			lUsers=$(lookupsid.py "$user":"$pass"@"$target" | tee /dev/tty | grep  "SidTypeUser" | awk -F '\' '{print $2}' | awk -F ' \\(' '{print $1}')
-			echo -e "$line"
+    if (( opt == 1 || opt == 2 )); then
+      if (( cred == 1 )); then
+        echo -e "\n[Local SIDs]\n"
+        lUsers=$(lookupsid.py "$user":"$pass"@"$target" | tee /dev/tty | grep  "SidTypeUser" | awk -F '\' '{print $2}' | awk -F ' \\(' '{print $1}')
+        echo -e "$line"
 		
-			if  (( ad == 1 ))
-			then
-				echo -e "\n[Domain SIDs]\n"
-				dUsers=$(lookupsid.py -domain-sids "$user":"$pass"@"$target" | tee /dev/tty | grep  "SidTypeUser" | awk -F '\' '{print $2}' | awk -F ' \\(' '{print $1}')
-			fi
+        if (( ad == 1 )); then
+          echo -e "\n[Domain SIDs]\n"
+          dUsers=$(lookupsid.py -domain-sids "$user":"$pass"@"$target" | tee /dev/tty | grep  "SidTypeUser" | awk -F '\' '{print $2}' | awk -F ' \\(' '{print $1}')
+        fi
 
-		elif (( cred == 2 ))
-		then
-			echo -e "\n[Local SIDs]\n"
-                        lUsers=$(lookupsid.py -hashes :$ntlm "$user"@"$target" | tee /dev/tty | grep  "SidTypeUser" | awk -F '\' '{print $2}' | awk -F ' \\(' '{print $1}')
-                        echo -e "$line"
+      elif (( cred == 2 )); then
+        echo -e "\n[Local SIDs]\n"
+        lUsers=$(lookupsid.py -hashes :$ntlm "$user"@"$target" | tee /dev/tty | grep  "SidTypeUser" | awk -F '\' '{print $2}' | awk -F ' \\(' '{print $1}')
+        echo -e "$line"
                 
-			if  (( ad == 1 ))
-                        then
-                                echo -e "\n[Domain SIDs]\n"
-                                dUsers=$(lookupsid.py -domain-sids -hashes :$ntlm "$user"@"$target" | tee /dev/tty | grep  "SidTypeUser" | awk -F '\' '{print $2}' | awk -F ' \\(' '{print $1}')
-                        fi
+        if  (( ad == 1 )); then
+          echo -e "\n[Domain SIDs]\n"
+          dUsers=$(lookupsid.py -domain-sids -hashes :$ntlm "$user"@"$target" | tee /dev/tty | grep  "SidTypeUser" | awk -F '\' '{print $2}' | awk -F ' \\(' '{print $1}')
+        fi
 
-		elif (( cred == 3 ))
-		then
-			echo -e "\n[Local SIDs]\n"
-			lUsers=$(lookupsid.py -no-pass "$user"@"$target" | tee /dev/tty | grep  "SidTypeUser" | awk -F '\' '{print $2}' | awk -F ' \\(' '{print $1}')
-			echo -e "$line"
+      elif (( cred == 3 )); then
+        echo -e "\n[Local SIDs]\n"
+        lUsers=$(lookupsid.py -no-pass "$user"@"$target" | tee /dev/tty | grep  "SidTypeUser" | awk -F '\' '{print $2}' | awk -F ' \\(' '{print $1}')
+        echo -e "$line"
 
-			if (( ad == 1 ))
-			then
-				echo -e "\n[Domain SIDs]\n"
-				dUsers=$(lookupsid.py -no-pass -domain-sids "$user"@"$target" | tee /dev/tty | grep  "SidTypeUser" | awk -F '\' '{print $2}' | awk -F ' \\(' '{print $1}')
-			fi
-		fi
+        if (( ad == 1 )); then
+          echo -e "\n[Domain SIDs]\n"
+          dUsers=$(lookupsid.py -no-pass -domain-sids "$user"@"$target" | tee /dev/tty | grep  "SidTypeUser" | awk -F '\' '{print $2}' | awk -F ' \\(' '{print $1}')
+        fi
+      fi
 	
-		if (( opt == 2 ))
-		then
-			echo -e "\nLocal Users: Path to output file\n"
-			read out1
-			echo -e "\nDomain Users: Path to output file (can be the same file path)\n"
-			read out2
+      if (( opt == 2 )); then
+        echo -e "\nLocal Users: Path to output file\n"
+        read out1
+        echo -e "\nDomain Users: Path to output file (can be the same file path)\n"
+        read out2
 
-			if [ "$out1" == "$out2" ]
-			then
-				for l in $lUsers
-                        	do
-                                	echo $l >> /dev/shm/smb_user_enum.txt
-                        	done
+        if [[ "$out1" == "$out2" ]]; then
+          for l in "$lUsers"; do
+            echo "$l" >> /dev/shm/smb_user_enum.txt
+          done
 
-                        	for d in $dUsers
-                        	do
-                                	echo $d >> /dev/shm/smb_user_enum.txt
-                        	done
+          for d in "$dUsers"; do
+            echo "$d" >> /dev/shm/smb_user_enum.txt
+          done
 
-				duplicut /dev/shm/smb_user_enum.txt -o $out1 	
+          duplicut /dev/shm/smb_user_enum.txt -o "$out1"
 
-			else
-				for l in $lUsers
-				do
-					echo $l >> $out1
-				done
+        else
+          for l in "$lUsers"; do
+            echo "$l" >> "$out1"
+          done
 
-				for d in $dUsers
-				do
-					echo $d >> $out2
-				done
-			fi
+          for d in "$dUsers"; do
+            echo "$d" >> "$out2"
+          done
+        fi
+      fi
+    else
+      echo -e "\nYou did not select a valid option\n"
+    fi
 
-		fi
-	else
-		echo -e "\nYou did not select a valid option\n"
-	fi
+  shares|3)
+    echo -e "\nEnter the Domain name or Hostname to use:\n"
+    read dom
+    echo -e "\nEnter the Username to use:\n"
+    read user
 
-elif (( mode == 3 ))
-then
-	echo -e "\nEnter the Domain name or Hostname to use:\n"
-	read dom
-	echo -e "\nEnter the Username to use:\n"
-	read user
+    echo -e "\n\n[!] Tip: You can authenticate with a password, or with LMHASH:NTHASH (to use hashes, you must specify both seperated with a colon as shown)\n"
+    echo -e "\nEnter the password or the hashes:\n"
+    read cred
+    smbmap -H $target -u "$user" -p "$cred" -d $dom -r
 
-	echo -e "\n\n[!] Tip: You can authenticate with a password, or with LMHASH:NTHASH (to use hashes, you must specify both seperated with a colon as shown)\n"
-	echo -e "\nEnter the password or the hashes:\n"
-	read cred
-
-#	set -x
-	smbmap -H $target -u "$user" -p "$cred" -d $dom -r
-#	set +x
-
-elif (( $mode == 4 ))
-then
-
+  enum|4)
 # May want to specify domain with -w ?
 #	echo -e "\nEnter the Domain name or Hostname to use:\n"
 #        read dom
-        echo -e "\n[!] Tip: If Null Sessions are supported, just enter a blank username & password\n\nEnter the Username to use:\n"
-        read user
+    echo -e "\n[!] Tip: If Null Sessions are supported, just enter a blank username & password\n\nEnter the Username to use:\n"
+    read user
 
-	echo -e "\nHow do you want to authenticate?\n[1] Password\n[2] NTLM Hash\n"
-	read cred
+    echo -e "\nHow do you want to authenticate?\n[1] Password\n[2] NTLM Hash\n"
+    read cred
 
-	if (( cred == 1 ))
-	then
-
-		echo -e "Enter the Password:\n"
-                read pass 
+    if (( cred == 1 )); then
+      echo -e "Enter the Password:\n"
+      read pass 
 		
-		enum4linux-ng -u "$user" -p "$pass" -R -d -A "$target"
+      enum4linux-ng -u "$user" -p "$pass" -R -d -A "$target"
 
-	elif (( cred == 2 ))
-	then
-		echo -e "\nEnter the NTLM hash:\n"
-                read ntlm
+    elif (( cred == 2 )); then
+      echo -e "\nEnter the NTLM hash:\n"
+      read ntlm
 
-		enum4linux-ng -u "$user" -H "$ntlm" -R -d -A "$target"
+      enum4linux-ng -u "$user" -H "$ntlm" -R -d -A "$target"
 
-	else
-		echo -e "\nYou did not select a valid option\n"
-                return
-        fi
+    else
+      echo -e "\nYou did not select a valid option\n"
+      return
+    fi
         
-else
-	echo -e "\nYou did not select a valid option\n"
-fi
+  *)
+    echo -e "\nYou did not select a valid option\n"
+    Help
+esac
