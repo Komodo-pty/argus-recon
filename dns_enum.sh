a1c10a541e6e0d6c0c27947d341f76a84a1cc53d
Nathan Miller <144389222+Komodo-pty@users.noreply.github.com>
2025-08-14 15:23:59 -0700

    Arguments for DNS module are now supported

--- dns_enum.sh
+++ dns_enum.sh
@@ -1,13 +1,44 @@
 #!/bin/bash
 
 line="\n============================================================\n"
-echo -e "[!] Tip: This queries a DNS Server, but it isn't a replacement for other DNS Enumeration techniques (e.g. fuzzing subdomains with ffuf)\n"
+hostname=""
+server=""
 
-echo -e "\nEnter the target's hostname:"
-read hostname
+Help(){
+cat <<EOF
+[Options]
+	-h: Display this help menu
+	-i <HOSTNAME>: The target's hostname
+	-s <DNS_SERVER>: The DNS Server's IP Address
 
-echo -e "\nEnter the DNS Server's IP Address:"
-read server
+[!] Tip: This queries a DNS Server, but it isn't a replacement for other DNS Enumeration techniques (e.g. fuzzing subdomains with ffuf)
+EOF
+exit 0
+}
+
+while getops ":hi:s:" option; do
+  case "$option" in
+    h)
+      Help
+      ;;
+    i)
+      hostname="$OPTARG"
+      ;;
+    s)
+      server="$OPTARG"
+      ;;
+  esac
+done
+
+if [[ -z "$hostname" ]]; then
+  echo -e "\nEnter the target's hostname:"
+  read hostname
+fi
+
+if [[ -z "$server" ]] ; then
+  echo -e "\nEnter the DNS Server's IP Address:"
+  read server
+fi
 
 echo -e "$line\n[Basic DNS Queries]\n$line\nLookup any records:\n"
 
@@ -20,5 +51,3 @@ dig all $hostname @$server
 echo -e "$line\n[AXFR]\n$line\n[!] Tip: If the output shows 'Transfer failed' it means that this type of query isn't supported by the DNS Server\n\nAttempting Zone Transfer:\n"
 
 dig axfr $hostname @$server
-
-echo -e "$line"
